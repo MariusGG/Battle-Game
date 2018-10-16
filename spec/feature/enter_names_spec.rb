53a251a61a1a7e6e37c0ff9591307cc131a64789
mariusgg <marius.g.guei@gmail.com>
2018-10-17 00:30:50 +0100

    added player HP

--- spec/feature/enter_names_spec.rb
+++ spec/feature/enter_names_spec.rb
@@ -3,5 +3,12 @@ Capybara.app = Battle
 feature "Start of the game" do
   scenario "players enter their names into the form" do
     sign_in_and_play
-    expect(page).to have_content "Player1 vs Player2"
+    expect(page).to have_content "Player1 vs. Player2"
   end
+end
+
+scenario "I can see player 2s hit points" do
+    sign_in_and_play
+    expect(page).to have_content "HP"
+  end
+end
