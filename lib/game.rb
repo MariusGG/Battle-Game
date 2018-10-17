5e4d54fe96a860d25bed219b057a9621d7a8e464
mariusgg <marius.g.guei@gmail.com>
2018-10-17 11:02:36 +0100

    added game class with attack method

--- lib/game.rb
+++ lib/game.rb
@@ -1,5 +1,15 @@
 class Game
 
+  attr_reader :player1, :player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def attack(opposition)
+    opposition.damage_point
+  end
 
 
 end
