6659cb6de2c0cd18419006fcd3915ef9d7154f31
mariusgg <marius.g.guei@gmail.com>
2018-10-17 10:53:04 +0100

    rewriten game spec test

--- lib/game.rb
+++ lib/game.rb
@@ -1,21 +1,5 @@
 class Game
 
-  attr_reader :current_player, :last_move
 
- def self.create(player1_name, player2_name, player_class = Player)
-   players = [player_class.new(player1_name), player_class.new(player2_name)]
-   new(players)
- end
-
-
-  def initialize(players)
-    @players = players
-    @current_player = 1
-    @last_move = nil
-  end
-  
-  def player_name(player_number)
-    player_number == 1 ? @players.character.name
-  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -1,15 +1,30 @@
 require 'game'
 
 describe Game do
+    let(:game) { describe_class.new(player1, player2) }
+    let(:player1) { double :player1 }
+    let(:player2) { double :player2 }
 
-  let(:player) { double :player, name: "Rosie"}
-  let(:player_class) { double :double, new: player }
+  context 'returns players' do
 
-  subject { described_class.create("Rosie", "Jim", player_class) }
+    describe "#get_player1_name" do
+      it "returns player1 name" do
+        expect(game.player1.to eq player1
+      end
+    end
 
-  describe "#get_player_name" do
-    it "returns player name" do
-      expect(subject.player_name(1)).to eq "Rosie"
+    describe "#get_player2_name" do
+      it "returns player2 name" do
+        expect(game.player2.to eq player2
+      end
+    end
+
+  end
+# this will get the method from the player class of :damage_point to be used in game
+  describe '#attack' do
+    it "attack a player" do
+      expect(player2).to recieve(:damage_point)
     end
   end
+
 end
